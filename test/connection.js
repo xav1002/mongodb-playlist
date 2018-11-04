373cfe46f9327ffc6668940f4c8de492ee20099d
xav1002 <orchdork.2002@gmail.com>
2018-11-04 11:28:17 -0600

    implementing dropping the connection before saving a new one every time

--- test/connection.js
+++ test/connection.js
@@ -7,7 +7,6 @@ mongoose.Promise = global.Promise;
 before(function(done) {
     // Connect to mongodb
     mongoose.connect('mongodb://localhost/testaroo');
-
     mongoose.connection.once('open', function() {
     console.log("Connection has been made, now make fireworks...");
     done();
@@ -15,4 +14,11 @@ before(function(done) {
     console.log("Connection error", error);
 });
 });
+// Drop the characters collection before each test
+beforeEach(function() {
+    // Drop the collection
+    mongoose.connection.collections.mariochars.drop(function(done) {
+        done();
+    });
+})
 
