9a7f3e4218297d4579fdbc15630b8bb43f26281a
xav1002 <orchdork.2002@gmail.com>
2018-11-03 23:30:12 -0500

    using saving data test with mocha example

--- test/demo_test.js
+++ test/demo_test.js
@@ -1,12 +1,21 @@
 const mocha = require('mocha');
 const assert = require('assert');
+const MarioChar = require('../models/marioChar');
 
 // Describe tests
-describe('some demo tests', function() {
+describe('Saving records', function() {
 
     // Create tests
-    it('adds two numbers together', function() {
-        assert(2 + 3 === 5);
+    it('Saves a records to the database', function(done) {
+        var char = new MarioChar({
+            name: 'Mario'
+        });
+
+        char.save().then(function() {
+            assert(char.isNew === false);
+            done();
+        });
+
     });
 
 });
