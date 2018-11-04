946bb8df1f531073350dc3c7a14cea4334c3e60f
xav1002 <orchdork.2002@gmail.com>
2018-11-03 23:31:19 -0500

    using saving data test with mocha example

--- test/demo_test.js
+++ test/demo_test.js
@@ -2,6 +2,8 @@ const mocha = require('mocha');
 const assert = require('assert');
 const MarioChar = require('../models/marioChar');
 
+// "C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="c:\data\db"
+
 // Describe tests
 describe('Saving records', function() {
 
