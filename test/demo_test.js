5c496b2546597aafc8ceb6e10a5c98e44599ef15
xav1002 <orchdork.2002@gmail.com>
2018-11-05 09:16:07 -0600

    update test example

--- test/demo_test.js
+++ test/demo_test.js
@@ -3,3 +3,11 @@ const MarioChar = require('../models/marioChar');
 
 // "C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="c:\data\db"
 
+// Make new records
+// Update records
+// Find records
+// Delete records
+// mocha tests
+// know how to set up connection to mongodb
+// update operators
+// pull data and display
--- test/update_record.js
+++ test/update_record.js
@@ -8,7 +8,8 @@ describe('Updating records', function() {
 
     beforeEach(function(done) {
         char = new MarioChar({
-            name: 'Mario'
+            name: 'Mario',
+            weight: 50
         });
 
         char.save().then(function() {
@@ -17,12 +18,21 @@ describe('Updating records', function() {
         });
     });
 
-    it('Update one record from the database', function(done) {
-        MarioChar.findOneAndUpdate({ name: 'Mario' }, { name: 'Luigi' }).then(function() {
-            MarioChar.findOne({ _id: char._id }).then(function(result) {
-                assert(result.name === 'Luigi');
-                done();
-            })
-        })
+    // it('Update one record from the database', function(done) {
+    //     MarioChar.findOneAndUpdate({ name: 'Mario' }, { name: 'Luigi' }).then(function() {
+    //         MarioChar.findOne({ _id: char._id }).then(function(result) {
+    //             assert(result.name === 'Luigi');
+    //             done();
+    //         });
+    //     });
+    // });
+
+it('Increment weight by 1', function() {
+    MarioChar.update({}, { $inc: {weight: 1} }).then(function(done) {
+        MarioChar.findOne({ name: 'Mario' }).then(function(record) {
+            assert(record.weight === 51);
+            done();
+        });
     });
+});
 });
